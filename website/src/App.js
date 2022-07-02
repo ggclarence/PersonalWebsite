1484744d246bc4ab66d683fcee3c5bef2a1373c4
Eric Van <ggclarence@yahoo.com>
2022-07-02 10:53:12 -0500

    organized files

--- website/src/App.js
+++ website/src/App.js
@@ -1,32 +1,12 @@
-import logo from './logo.svg';
-import NavBar from './NavBar';
+import NavBar from './components/Nav/NavBar.js';
 import './App.css';
 
 function App() {
   return (
-    // <div className="App">
-    //   <header className="App-header">
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //     <p>
-    //       Edit <code>src/App.js</code> and save to reload.
-    //     </p>
-    //     <a
-    //       className="App-link"
-    //       href="https://reactjs.org"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       Learn React
-    //     </a>
-    //   </header>
-    // </div>
-    
-    <div>
-      <h1 className='My Website'>3</h1>
+    <div className='AppContainer'>
+      <h1 className='My Website'>Welcome</h1>   
       <NavBar/>
-      <NavBar/>
-      <NavBar/>
-
+      
     </div>
   );
 }
